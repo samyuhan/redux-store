0b6ed5c097b18567e37ae85910083d6441ac2382
Samantha Yuhan <sysweettooth1@gmail.com>
2021-09-01 22:39:24 -0700

    deploy to heroku

---
+++ client/build/precache-manifest.2ec69a9a781d7c656fcf474eebd71df3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f98375f48cf16581555a51d63278f40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "682344ecaf4939395186",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "ad2d0c7f50d6312de997",
+    "url": "/static/js/2.5909e8e3.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.5909e8e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "682344ecaf4939395186",
+    "url": "/static/js/main.70b12991.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
